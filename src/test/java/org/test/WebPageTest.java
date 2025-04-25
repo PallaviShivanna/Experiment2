03a9a389a7b84b47358f5e56a54cc3f443d6790d
PallaviShivanna <pallavishivanna0123@gmail.com>
2025-04-25 10:53:31 +0530

    commit

---
+++ src/test/java/org/test/WebPageTest.java
@@ -0,0 +1,32 @@
+package org.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+public class WebPageTest {
+        private static WebDriver driver;
+
+        @BeforeTest
+        public void openBrowser() throws InterruptedException {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            Thread.sleep(2000);
+            driver.get("https://pallavishivanna.github.io/Experiment2/");
+        }
+        @Test
+        public void titleValidationTest(){
+            String actualTitle = driver.getTitle();
+            System.out.println("Page Title:"+actualTitle);
+            String expectedTitle = "Colorful Login Page";
+            Assert.assertEquals(actualTitle, expectedTitle, "Page title doesn't match!");
+            assertTrue(actualTitle.contains("Login"), "Title should contain 'Design'");
+        }
+        @AfterTest
+        public void closeBrowser() throws InterruptedException {
+            Thread.sleep(2000);
+            driver.quit();
+        }
+}
